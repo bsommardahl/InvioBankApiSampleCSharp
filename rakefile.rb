0d5d56ff10f631c78472ed566196a5de291130e3
Byron Sommardahl <byron@acklenavenue.com>
2014-06-28 09:57:39 -0500

    Fixed rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -14,7 +14,7 @@ CONFIG = "Debug"
 
 task :default => [:all]
 
-task :all => [:removeArtifacts, :compile, :specs, :dbUpdate, :publish]
+task :all => [:removeArtifacts, :compile]
 
 task :removeArtifacts do
 	require 'fileutils'
@@ -24,73 +24,7 @@ task :removeArtifacts do
 	FileUtils.rm_rf DATABASE_DEPLOYMENT_PATH	
 end
 
-task :publish => [:compile] do
-	puts 'Creating deployment artifact...'	
-	Dir.chdir("build/_publishedWebsites/#{WEB_APP}") do
-		sh "\"#{PATH_7ZIP}\" a \"../../#{WEB_APP}.zip\" *"
-	end
-end
-	
 task :compile do
 	puts 'Compiling solution...'
 	sh "#{MSBUILD_PATH} /p:Configuration=#{CONFIG} /p:OutDir=\"#{BUILD_PATH}/\" /p:PostBuildEvent=\"\" #{SOLUTION_PATH}"	
-end
-
-task :specs do
-    puts "Locating and running MSpec tests in #{BUILD_PATH}."
-    puts 'This can take a while, please wait...'
-    
-    # Create output directory if necessary
-    mkdir REPORTS_PATH unless File.exist?(REPORTS_PATH)
-    
-    # Clear out old reports, but don't talk about it.
-    verbose(false) do
-        FileList.new("#{REPORTS_PATH}/*").each { |file| rm file }
-    end
-    
-    # Find all the specs DLLs
-    testDlls = FileList.new("#{BUILD_PATH}/*.Specs.dll")
-    
-    # Join the file list into a single string with quotes surrounding the file paths
-    TEST_DLL_LIST = '"' + testDlls.join('" "') + '"'
-    
-    # Specify our mspec command line parameters
-    HTML_SWITCH = "--html \"#{REPORTS_PATH}\""
-    XML_SWITCH = "--xml \"#{REPORTS_PATH}\\testresults.xml\""
-    
-    # Run the tests and give a message on failure
-    # But we don't want to hear the chatter, so dump the console output to a file
-    sh "#{MSPEC_PATH} #{HTML_SWITCH} #{XML_SWITCH} #{TEST_DLL_LIST}"
-    result = $?.to_i
-    
-    raise "One or more tests failed.  Please see results in #{REPORTS_PATH}\\index.html" unless result == 0
-
-    # If we get here, all's good
-    puts "All tests passed"
-end
-
-task :dbRebuild => [:dbDrop, :dbCreate, :dbSeed]
-
-task :dbDrop => [:compile] do
-	Dir.chdir("build/") do
-		sh "DatabaseDeployer.exe drop"
-	end
-end
-
-task :dbCreate => [:compile] do
-	Dir.chdir("build/") do
-		sh "DatabaseDeployer.exe create"
-	end
-end
-
-task :dbUpdate => [:compile] do
-	Dir.chdir("build/") do
-		sh "DatabaseDeployer.exe update"
-	end
-end
-
-task :dbSeed => [:compile] do
-	Dir.chdir("build/") do
-		sh "DatabaseDeployer.exe seed"
-	end
 end
